16bf60aaa63e765c478b44793daa3b231f76f383
sungchun12 <sungwonchung3@gmail.com>
2023-03-19 14:02:15 -0700

    validations for click

--- fst/main.py
+++ fst/main.py
@@ -1,17 +1,28 @@
 import click
 from fst import fst_query
 
+
 @click.group()
 def main():
     pass
 
+
 @main.command()
-@click.option("--file-path", type=click.Path(exists=True), help="Path to the SQL file you want to watch.")
+@click.option(
+    "--file-path",
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+    help="Path to the SQL file you want to watch.",
+)
 def start(file_path):
     if file_path:
-        fst_query.watch_directory(fst_query.CURRENT_WORKING_DIR, fst_query.handle_query, file_path)
+        fst_query.watch_directory(
+            fst_query.CURRENT_WORKING_DIR, fst_query.handle_query, file_path
+        )
     else:
         click.echo("Please provide a file path using the --file-path option.")
+
 
 @main.command()
 def stop():
@@ -22,8 +33,15 @@ def stop():
     else:
         click.echo("No observer is currently running.")
 
+
 @main.command()
-@click.option("--file-path", type=click.Path(exists=True), help="Path to the SQL file you want to watch.")
+@click.option(
+    "--file-path",
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+    help="Path to the SQL file you want to watch.",
+)
 def restart(file_path):
     if fst_query.observer:
         fst_query.observer.stop()
@@ -31,11 +49,14 @@ def restart(file_path):
         click.echo("Stopped watching the directory.")
     else:
         click.echo("No observer was running. Starting a new one.")
-    
+
     if file_path:
-        fst_query.watch_directory(fst_query.CURRENT_WORKING_DIR, fst_query.handle_query, file_path)
+        fst_query.watch_directory(
+            fst_query.CURRENT_WORKING_DIR, fst_query.handle_query, file_path
+        )
     else:
         click.echo("Please provide a file path using the --file-path option.")
+
 
 if __name__ == "__main__":
     main()
