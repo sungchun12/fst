353b0a73e977807a685b1c88ad833fbc9e002c94
Sung Won Chung <sungwonchung3@gmail.com>
2023-03-22 11:09:42 -0700

    robust path flagging

--- fst/main.py
+++ fst/main.py
@@ -14,7 +14,15 @@ def main():
 
 
 @main.command()
-@click.option("--path", "-p", default=CURRENT_WORKING_DIR, help="dbt project root directory")
+@click.option(
+    "--path",
+    "-p",
+    default=CURRENT_WORKING_DIR,
+    type=click.Path(
+        exists=True, dir_okay=True, readable=True, resolve_path=True
+    ),
+    help="dbt project root directory. Defaults to current working directory.",
+)
 def start(path):
     project_dir = path
     models_dir = get_models_directory(project_dir)
