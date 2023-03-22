bec6391e97965aa8363f61c53c28b9e37c9588b1
Sung Won Chung <sungwonchung3@gmail.com>
2023-03-22 10:12:34 -0700

    add path option

--- fst/main.py
+++ fst/main.py
@@ -1,12 +1,11 @@
 import os
-import time
 import click
-from watchdog.observers.polling import PollingObserver
 
 from fst.file_utils import get_models_directory
 from fst.query_handler import handle_query, DynamicQueryHandler
 from fst.directory_watcher import watch_directory
 from fst.logger import setup_logger
+from fst.constants import CURRENT_WORKING_DIR
 
 
 @click.group()
@@ -15,9 +14,10 @@ def main():
     pass
 
 
-@main.command()
-def start():
-    project_dir = os.path.abspath(".")
+@main.command() #TODO: add file path flag
+@click.option("--path", "-p", default=CURRENT_WORKING_DIR, help="dbt project root directory")
+def start(path):
+    project_dir = path
     models_dir = get_models_directory(project_dir)
 
     event_handler = DynamicQueryHandler(handle_query, models_dir)
