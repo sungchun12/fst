55218c010e3fa4628b146c6454a4c4dbdb4e4a75
Sung Won Chung <sungwonchung3@gmail.com>
2023-03-24 14:50:07 -0700

    dynamic metrics

--- fst/streamlit_duckdb.py
+++ fst/streamlit_duckdb.py
@@ -18,25 +18,28 @@ st.title("fst Performance and Productivity")
 # Show the fst_metrics data
 st.write("## Result Preview")
 index_options = [f"{row.timestamp} - {row.modified_sql_file}" for _, row in metrics_df.iterrows()]
-latest_section = len(index_options) - 1
-selected_option = st.selectbox("Select a row to display the result preview:", options=index_options, index=latest_section)
+selected_option = st.selectbox("Select a row to display the result preview:", options=index_options, index=len(index_options) - 1)
 selected_index = index_options.index(selected_option)
 selected_row = metrics_df.iloc[selected_index]
 result_preview_df = pd.read_json(selected_row["result_preview_json"])
 st.write(result_preview_df)
 
-# Calculate average dbt build time and average query time
-average_dbt_build_time = metrics_df["dbt_build_time"].mean()
-average_query_time = metrics_df["query_time"].mean()
+# Filter metrics_df based on the selected option
+filtered_metrics_df = metrics_df[metrics_df["modified_sql_file"] == selected_row["modified_sql_file"]]
+
+# Calculate average dbt build time and average query time for the selected option
+average_dbt_build_time = filtered_metrics_df["dbt_build_time"].mean()
+average_query_time = filtered_metrics_df["query_time"].mean()
 
 # Performance Metrics
 st.write("## Performance Metrics")
-st.write(f"Average `dbt build` time: {average_dbt_build_time:.2f} seconds")
-st.write(f"Average query time: {average_query_time:.2f} seconds")
+st.write(f"Average `dbt build` time for the selected option: {average_dbt_build_time:.2f} seconds")
+st.write(f"Average query time for the selected option: {average_query_time:.2f} seconds")
 
 # Visualizations
 st.write("## Visualizations")
-st.bar_chart(metrics_df["dbt_build_time"])
+st.bar_chart(filtered_metrics_df["dbt_build_time"])
+
 
 # Display contents of the compiled_sql_file
 st.write("## Compiled SQL File")
