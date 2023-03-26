173bf0babd683f23c7d20457d84cd70190ff2838
sungchun12 <sungwonchung3@gmail.com>
2023-03-25 18:44:05 -0700

    toggle button

--- fst/streamlit_duckdb.py
+++ fst/streamlit_duckdb.py
@@ -9,6 +9,7 @@ def fetch_metrics_data():
     duckdb_conn.close()
     return metrics_df
 
+
 # Fetch the metrics data
 metrics_df = fetch_metrics_data()
 
@@ -17,15 +18,23 @@ st.title("fst Performance and Productivity")
 
 # Show the fst_metrics data
 st.write("## Result Preview")
-index_options = [f"{row.timestamp} - {row.modified_sql_file}" for _, row in metrics_df.iterrows()]
-selected_option = st.selectbox("Select a row to display the result preview:", options=index_options, index=len(index_options) - 1)
+index_options = [
+    f"{row.timestamp} - {row.modified_sql_file}" for _, row in metrics_df.iterrows()
+]
+selected_option = st.selectbox(
+    "Select a row to display the result preview:",
+    options=index_options,
+    index=len(index_options) - 1,
+)
 selected_index = index_options.index(selected_option)
 selected_row = metrics_df.iloc[selected_index]
 result_preview_df = pd.read_json(selected_row["result_preview_json"])
 st.write(result_preview_df)
 
 # Filter metrics_df based on the selected option
-filtered_metrics_df = metrics_df[metrics_df["modified_sql_file"] == selected_row["modified_sql_file"]]
+filtered_metrics_df = metrics_df[
+    metrics_df["modified_sql_file"] == selected_row["modified_sql_file"]
+]
 
 # Calculate average dbt build time and average query time for the selected option
 average_dbt_build_time = filtered_metrics_df["dbt_build_time"].mean()
@@ -33,8 +42,12 @@ average_query_time = filtered_metrics_df["query_time"].mean()
 
 # Performance Metrics
 st.write("## Performance Metrics")
-st.write(f"Average `dbt build` time for the selected option: {average_dbt_build_time:.2f} seconds")
-st.write(f"Average query time for the selected option: {average_query_time:.2f} seconds")
+st.write(
+    f"Average `dbt build` time for the selected option: {average_dbt_build_time:.2f} seconds"
+)
+st.write(
+    f"Average query time for the selected option: {average_query_time:.2f} seconds"
+)
 
 # Visualizations
 st.write("## Visualizations")
@@ -43,8 +56,13 @@ st.bar_chart(filtered_metrics_df["dbt_build_time"])
 # Display contents of the compiled_sql_file
 st.write("## Compiled SQL File")
 
-# Toggle button for showing/hiding the code snippet
-show_code = st.checkbox("Show/hide code snippet", value=False)
+# Aesthetically pleasing toggle button
+query_params = st.experimental_get_query_params()
+show_code = query_params.get("show_code", ["False"])[0].lower() == "true"
+
+if st.button("Toggle code snippet"):
+    show_code = not show_code
+    st.experimental_set_query_params(show_code=show_code)
 
 if show_code:
     with open(selected_row["compiled_sql_file"], "r") as f:
