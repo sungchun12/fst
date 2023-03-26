f668d382be0d4073fa67e71f417be68255c82ca7
sungchun12 <sungwonchung3@gmail.com>
2023-03-25 21:58:13 -0700

    fixed warnings

--- fst/streamlit_duckdb.py
+++ fst/streamlit_duckdb.py
@@ -10,6 +10,7 @@ def fetch_metrics_data():
     duckdb_conn.close()
     return metrics_df
 
+
 # Fetch the metrics data
 metrics_df = fetch_metrics_data()
 
@@ -17,7 +18,7 @@ metrics_df = fetch_metrics_data()
 st.title("fst Performance and Productivity")
 
 # Show the fst_metrics data
-st.write("## Result Preview")
+st.write("### Result Preview")
 index_options = [
     f"{row.timestamp} - {row.modified_sql_file}" for _, row in metrics_df.iterrows()
 ]
@@ -32,16 +33,16 @@ result_preview_df = pd.read_json(selected_row["result_preview_json"])
 st.write(result_preview_df)
 
 # Filter metrics_df based on the selected option
-filtered_metrics_df = metrics_df[
+filtered_metrics_df = metrics_df.loc[
     metrics_df["modified_sql_file"] == selected_row["modified_sql_file"]
-]
+].copy()  # Create a copy of the DataFrame to avoid SettingWithCopyWarning
 
 # Calculate average dbt build time and average query time for the selected option
 average_dbt_build_time = filtered_metrics_df["dbt_build_time"].mean()
 average_query_time = filtered_metrics_df["query_time"].mean()
 
 # Performance Metrics
-st.write("## Performance Metrics")
+st.write("### Performance Metrics")
 st.write(
     f"Average `dbt build` time for the selected option: {average_dbt_build_time:.2f} seconds"
 )
@@ -50,25 +51,39 @@ st.write(
 )
 
 # Calculate rolling average
-filtered_metrics_df["rolling_average"] = filtered_metrics_df["dbt_build_time"].rolling(window=len(filtered_metrics_df), min_periods=1).mean()
+filtered_metrics_df.loc[:, "rolling_average"] = (
+    filtered_metrics_df["dbt_build_time"]
+    .rolling(window=len(filtered_metrics_df), min_periods=1)
+    .mean()
+)
 
 # Reset the index of the filtered_metrics_df
 filtered_metrics_df = filtered_metrics_df.reset_index()
 
 # Create a line chart for the rolling average
-fig = px.line(filtered_metrics_df, x='index', y='rolling_average', labels={'index': 'Index', 'rolling_average': 'Rolling Average'})
+fig = px.line(
+    filtered_metrics_df,
+    x="index",
+    y="rolling_average",
+    labels={"index": "# of Modifications", "rolling_average": "Time in Seconds"},
+)
 
 # Add a bar chart for dbt build times
-fig.add_bar(x=filtered_metrics_df['index'], y=filtered_metrics_df['dbt_build_time'], name='DBT Build Time')
+fig.add_bar(
+    x=filtered_metrics_df["index"],
+    y=filtered_metrics_df["dbt_build_time"],
+    name="dbt build Time",
+)
 
 # Plot the combined chart
 st.write(fig)
+
 # Calculate the number of modifications per file and average performance stats related to each file
 modifications_per_file = (
     metrics_df.groupby("modified_sql_file").size().reset_index(name="num_modifications")
 )
 average_performance_stats = (
-    metrics_df.groupby("modified_sql_file")["dbt_build_time", "query_time"]
+    metrics_df.groupby("modified_sql_file")[["dbt_build_time", "query_time"]]
     .mean()
     .reset_index()
 )
@@ -76,20 +91,18 @@ average_performance_stats = average_performance_stats.rename(
     columns={"dbt_build_time": "avg_dbt_build_time", "query_time": "avg_query_time"}
 )
 file_modifications_and_performance = pd.merge(
-    modifications_per_file, average_performance_stats, on="modified_sql_file"
+    modifications_per_file, average_performance_stats
 )
-
 # Display the number of modifications per file and average performance stats in a simple dataframe
-st.write("## File Modifications and Average Performance Stats")
+st.write("### File Modifications and Average Performance Stats")
 st.write(file_modifications_and_performance)
 
 # Display contents of the compiled_sql_file
-st.write("## Compiled SQL File")
 # Aesthetically pleasing toggle button
 query_params = st.experimental_get_query_params()
 show_code = query_params.get("show_code", ["False"])[0].lower() == "true"
 
-if st.button("Toggle code snippet"):
+if st.button("Toggle code snippet for selected option"):
     show_code = not show_code
     st.experimental_set_query_params(show_code=show_code)
 
