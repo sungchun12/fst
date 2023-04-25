1971ed3dc2663b4f3bafa65a3c52e2450070855a
tomflo <tomaszflorek1@gmail.com>
2023-04-25 23:17:17 +0200

    refactor(config_defaults.py): remove unused CONFIG variable

--- fst/config_defaults.py
+++ fst/config_defaults.py
@@ -2,9 +2,7 @@ import os
 import yaml
 
 CURRENT_WORKING_DIR = os.getcwd()
-CONFIG = {
-    "PREVIEW_LIMIT_ROWS": 5
-}
+
 # Load profiles.yml only once
 profiles_path = os.path.join(CURRENT_WORKING_DIR, "profiles.yml")
 with open(profiles_path, "r") as file:
